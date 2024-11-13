4c7014afbd816650a392c5bc9819a6913f4e0a11
ostapenko-kostia <ostapenkokpersonal@gmail.com>
2024-11-13 20:30:29 +0200

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,36 @@
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
   return (
     <footer className="w-full bg-white rounded-t-[20px] pb-[44px] pt-[39px] h-[150px] fixed bottom-0 left-0 z-10">
       <div className="flex justify-center items-center gap-[28px]">
-        <div className="flex flex-col items-center">
+        <Link to="/" className="flex flex-col items-center">
           <img className="w-[48px] h-[48px]" src="./img/Frame 310.svg" alt="" />
           <p className="text-black text-[12px] leading-[167%] font-medium">
             Домашняя
           </p>
-        </div>
+        </Link>
 
-        <div className="flex flex-col items-center">
+        <Link to="/subscription" className="flex flex-col items-center">
           <img className="w-[48px] h-[48px]" src="./img/Frame 311.svg" alt="" />
           <p className="text-black text-[12px] leading-[167%] font-medium">
             Подписка
           </p>
-        </div>
+        </Link>
 
-        <div className="flex flex-col items-center">
+        <Link to='/bonus' className="flex flex-col items-center">
           <img className="w-[48px] h-[48px]" src="./img/Frame 312.svg" alt="" />
           <p className="text-black text-[12px] leading-[167%] font-medium">
             Бонусы
           </p>
-        </div>
+        </Link>
 
-        <div className="flex flex-col items-center">
+        <Link to='/profile' className="flex flex-col items-center">
           <img className="w-[48px] h-[48px]" src="./img/Frame 313.svg" alt="" />
           <p className="text-black text-[12px] leading-[167%] font-medium">
             Профиль
           </p>
-        </div>
+        </Link>
       </div>
     </footer>
   );
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "../Footer";
 import HomeBook from "./HomeBook";
 import HomeVideo from "./HomeVideo";
 
@@ -6,6 +7,8 @@ export const Home = () => {
     <div className="px-[30px] py-[60px] overflow-visible w-full h-screen bg-gradient-to-b from-[#6ec1e4] to-[#b3e5fc] flex flex-col items-center gap-11">
       <HomeVideo videoUrl="video.mp4" />
       <HomeBook />
+
+      <Footer />
     </div>
   );
 };
