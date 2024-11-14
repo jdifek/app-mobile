a1939d23c74505541ad0c4da10b6b901cd69bf9e
Den <clixqqk@gmail.com>
2024-11-14 21:35:35 +0200

    fdsfds

--- src/types/telegram.d.ts
+++ src/types/telegram.d.ts
@@ -1,11 +1,31 @@
-export interface TelegramWebApp {
-  initData: any;
-}
-
 declare global {
+  interface TelegramWebApp {
+    MainButton: {
+      text: string;
+      show: () => void;
+      hide: () => void;
+      onClick: (callback: () => void) => void;
+    };
+    initData: string;
+    initDataUnsafe: {
+      user?: {
+        first_name: string;
+        last_name?: string;
+        username?: string;
+      };
+    };
+    themeParams: {
+      bg_color?: string;
+      text_color?: string;
+    };
+    close: () => void;
+  }
+
   interface Window {
     Telegram: {
       WebApp: TelegramWebApp;
     };
   }
 }
+
+export {};
