78128cc73de6547f1303af10229212e9f04e67d8
Den <clixqqk@gmail.com>
2024-11-15 16:48:55 +0200

    fdgf

---
+++ src/components/Assignments/ListenToASychewa.tsx
@@ -0,0 +1,26 @@
+export const ListenToASychewa = () => {
+  return (
+    <div className="w-full h-screen">
+      <div
+        className="px-[55px] py-[35px] w-full h-[40vh] rounded-tl-[20px] rounded-tr-[20px] rounded-bl-none rounded-br-none absolute bottom-0 flex flex-col items-center justify-center bg-gradient-to-b from-[#6ec1e4] to-[#b3e5fc]"
+        style={{
+          background:
+            "radial-gradient(119.41% 109.58% at 96.62% 100%, rgb(103, 198, 255) 0%, rgb(18, 124, 241) 63.837021589279175%, rgb(16, 118, 229) 100%)",
+        }}
+      >
+        <h1 className="text-white text-center text-[24px] leading-[150%] tracking-[0.02em] font-semibold">
+          Подпишись на канал <br></br> «o.sychewa»
+        </h1>
+
+        <div className="flex flex-col gap-4 mt-4">
+          <button
+            className="
+         h-[54px]  text-white text-center text-[16px] font-semibold bg-gradient-to-r from-[#00b4ff] to-[#007cff] rounded-full px-6 py-2"
+          >
+            Перейти в канал
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
